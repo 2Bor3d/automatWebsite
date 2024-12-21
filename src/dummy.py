dd3e4ab9c162a84244167042b70b37b621e60c86
2bor3d <david.glaenzel@gmail.com>
2024-12-21 18:08:40 +0100

    added a bit of css

--- src/dummy.py
+++ src/dummy.py
@@ -22,10 +22,29 @@ def script():
             return file.read();
 
 
+@app.route("/styles.css")
+def styles():
+    if flask.request.cookies.get("auth") == "true":
+        with open("styles.css", "r") as file:
+            return flask.Response(file.read(), mimetype="text/css");
+    else:
+        with open("login/styles.css", "r") as file:
+            return file.read();
+
+
 @app.route("/page.html")
 def page():
     if flask.request.cookies.get("auth") == "true":
-        with open("script.js", "r") as file:
+        with open("list/index.html", "r") as file:
+            return file.read();
+    else:
+        return flask.make_response("authorisation failed"), 403
+
+
+@app.route("/page/script.js")
+def pageJs():
+    if flask.request.cookies.get("auth") == "true":
+        with open("list/script.js", "r") as file:
             return file.read();
     else:
         return flask.make_response("authorisation failed"), 403
@@ -37,6 +56,12 @@ def login():
     response = flask.make_response("success");
     response.set_cookie("auth", "true");
     return response;
+
+
+@app.route("/entrys", methods=["POST"])
+def entrys():
+    return [{"first": "david", "last": "glaenzel", "attendence": ["21", "22"], "time": 5.3},
+            {"first": "ben", "last": "schnorri", "attendence": ["32", "54"], "time": 1.2}]
 
 
 if __name__ == "__main__":
